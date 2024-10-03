e07e290346003ab13413ca99724dea80235c720c
sakash5940 <akashpatil5940@gmail.com>
2024-10-03 17:00:32 +0530

    Add files via upload

---
+++ switch case.js
@@ -0,0 +1,87 @@
+//question1
+var a="friday"
+switch (a) {
+    case"monday":
+    console.log("its monday")
+    break;
+    case"tuesday":
+    console.log("its tuesday")
+    break;
+    case "wednesday":
+    console.log("its wednesday")
+    break;
+    case"thursday":
+    console.log("its thursday")
+    break;
+    case"friday":
+    console.log("its friday")
+    break;
+    case"saturday":
+    console.log("its saturday")
+    break;
+    case"sunday":
+    console.log("its sunday")
+        break;
+    default:("non of the above")
+        break;
+}
+
+//question2
+var b="october"
+switch (b) {
+    case"january":
+    console.log("first month ")
+    break;
+    case"february":
+    console.log("second month")
+    break;
+    case "march":
+    console.log("third month")
+    break;
+    case"april":
+    console.log("fourth month")
+    break;
+    case"may":
+    console.log("fifth month")
+    break;
+    case"june":
+    console.log("sixth month")
+    break;
+    case"july":
+    console.log("seventh month")
+    case "august":
+            console.log("eightth month")
+            break;
+            case"september":
+            console.log("nineth month")
+            break;
+            case"october":
+            console.log("tenth month")
+            break;
+            case"november":
+            console.log("eleventhsaturday")
+            break;
+            case"december":
+            console.log("twelveth sunday")
+                break;
+            default:("non of the above")
+                break;
+        
+}
+//question3
+a=1
+while(a<=10){
+    if(a%2==0){
+        console.log("its even number")
+    }else{
+        console.log("its an odd number")
+
+    }
+    a++
+}
+//question4
+i=10
+while(i>=1){
+    console.log("the game start in "+i)
+    i--
+}
